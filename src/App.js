a2e60b6567eca4a4a44fb49b0f55d43824769c67
정진오 <78059088+Jinoh-Jeong@users.noreply.github.com>
2022-04-25 14:01:49 +0900

    Draggable 수정

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import { useRef } from "react";
 import Draggable from "./lib/Draggable";
-
-
-
 function App() {
-
   const handle = useRef(null)
   return (
-    
-     <Draggable handleRef={handle} onMove={(x,y) => console.log(x,y)}>
-        <div style={{width:"100px" ,height:"100px", backgroundColor:"greenyellow"}}>
-          <button ref={handle}>
-            Move
-          </button>
-        </div>
-
-     </Draggable>
-    
+   <Draggable handleRef={handle} onMove={(x,y)=>console.log(x,y)}>
+     <div style={{width:"100px",height:"100px",backgroundColor:"greenyellow"}}>
+        <button ref={handle}>Move</button>
+     </div>
+   </Draggable>
   );
 }
 
